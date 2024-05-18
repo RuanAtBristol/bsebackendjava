f15e6b14e8850951175db354a50cffcbf367f3e7
pyfinart <mypyfinart@gmail.com>
2024-05-18 17:29:17 +0800

    backend prototype finished

---
+++ sql/create_table.sql
@@ -0,0 +1,36 @@
+# 数据库初始化
+# @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+# @from <a href="https://yupi.icu">编程导航知识星球</a>
+
+-- 创建库
+create database if not exists bse_temp;
+
+-- 切换库
+use bse_temp;
+
+-- 用户表
+create table if not exists user
+(
+    id           bigint auto_increment comment 'id' primary key,
+    userAccount  varchar(256)                           not null comment '账号',
+    userPassword varchar(512)                           not null comment '密码',
+    userName     varchar(256)                           null comment '用户昵称',
+    userRole     varchar(256) default 'user'            not null comment '用户角色：user/admin',
+    createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete     tinyint      default 0                 not null comment '是否删除'
+) comment '用户' collate = utf8mb4_unicode_ci;
+
+create table if not exists chart
+(
+    id         bigint auto_increment comment 'id' primary key,
+    goal       varchar(256)                       null comment '分析目标',
+    chartData  text null comment 'chart data',
+    chartType  varchar(128)                       null comment '图表类型',
+    genChart   text null comment '生成的图表数据',
+    genResult  text null comment '生成的分析结论',
+    userId      bigint                            null comment '创建图标的用户的id',
+    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete   tinyint  default 0                 not null comment '是否删除'
+)comment '图表信息表' collate = utf8mb4_unicode_ci;
---
+++ src/main/java/com/yupi/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.springbootinit.mapper;
+
+import com.yupi.springbootinit.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author yhj19
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2024-05-18 00:04:33
+* @Entity com.yupi.springbootinit.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
